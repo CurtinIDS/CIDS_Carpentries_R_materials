26409fd550bc87eb1c5ad68b3267b6bbcc8c829f
kathrynnapier <kathryn.napier@curtin.edu.au>
2025-03-10 16:54:34 +0800

    work in progress to update ecology lessons

--- binder/install.R
+++ binder/install.R
@@ -1,4 +1,7 @@
+# install.packages("tidyverse")
+# install.packages("rmarkdown")
+# install.packages("patchwork")
+# install.packages("gridExtra")
+
 install.packages("tidyverse")
-install.packages("rmarkdown")
-install.packages("patchwork")
-install.packages("gridExtra")
+install.packages("ratdat")
