57851e2facb24670226143e14cb7a0fed5a7fc08
kathrynnapier <kathryn.napier@curtin.edu.au>
2025-03-20 14:25:16 +0800

    updated packages to install

--- binder/install.R
+++ binder/install.R
@@ -1,8 +1,3 @@
-# install.packages("tidyverse")
-# install.packages("rmarkdown")
-# install.packages("patchwork")
-# install.packages("gridExtra")
-
 install.packages("tidyverse")
 install.packages("ratdat")
 install.packages("plotly")
